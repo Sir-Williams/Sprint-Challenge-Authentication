d0971a9b1b0bdfc7cd02a0469191593d0bdf181e
Sir-Williams <sirjohnwilliams@live.com>
2019-09-13 16:46:17 -0700

    added .env for port; added auth middleware

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -1,5 +1,12 @@
-
+/* 
+  complete the middleware code to check if the user is logged in
+  before granting access to the next middleware/route handler
+*/
 
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+  if(req.session && req.session.user) {
+      next();
+  } else {
+      res.status(401).json({ message: 'Not Valid Credentials'})
+  }
 };
